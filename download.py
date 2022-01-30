45fab3ea067eec16300aad651f134fb33c9cb1fc
whitel <heriskent@gmail.com>
2022-01-30 21:55:47 +0800

    update result filename

--- download.py
+++ download.py
@@ -3,21 +3,32 @@ import os
 from lxml import etree
 from bs4 import BeautifulSoup as bs
 import datetime
+import hashlib
 
 prefix = "./htmls/"
 
 def download(filename):
+    # read local html contents
     with open(prefix + filename, "r") as f:
         soup = bs(f.read(), features="lxml")
+
+    # get actual video address
     src = soup.find("video").contents[0]["src"]
     src = "https:" + src
+
+    # get video contents
     title = soup.find("meta",{"data-react-helmet": "true", "name": "description"})["content"]
     print("[v] Downloading:  " + title)
-    with open("./result/" + str(datetime.datetime.now()) + ".mp4", "wb") as f:
-        response = requests.get(src)
-        f.write(response.content)
+    response = requests.get(src)
+    video = response.content
+
+    # write to local file
+    author = soup.find("div", {"class": "account-name"}).text
+    video_hash = hashlib.sha1(video)
+    with open("./result/" + author + "-" + video_hash.hexdigest() + ".mp4", "wb") as f:
+        f.write(video)
     print("[v] Done")
-    print("=======================================")
+    print("====================================================")
 
 if __name__ == '__main__':
     files = os.listdir(prefix)
