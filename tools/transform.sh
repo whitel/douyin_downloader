12a0128e2cdccd369ea30d1f6792ec11a1bc7da1
whitel <heriskent@gmail.com>
2022-01-31 14:13:12 +0800

    Update

--- tools/transform.sh
+++ tools/transform.sh
@@ -1,1 +1,1 @@
-sed -e '1d' -e '$d' -e 's/  -H //g' -e "s/:/':/g" -e "s/: /: '/g" -e 's/ \\$/,/g' ./data/curl_headers | sed -e '$s/,//g' | sed -e '1iheaders = {' -e '$a}' > ./data/headers.py
+sed -e '1d' -e '$d' -e 's/  -H //g' -e "s/:/':/" -e "s/: /: '/" -e 's/ \\$/,/g' ./data/curl_headers | sed -e '$s/,//g' | sed -e '1iheaders = {' -e '$a}' > ./data/headers.py
